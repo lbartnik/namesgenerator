bb5b1394a8119f5a561baf09ae3e361a5d5d7b71
Lukasz A. Bartnik <l.bartnik@gmail.com>
2018-08-25 11:37:57 -0700

    add tests

--- tests/testthat/test-generator.R
+++ tests/testthat/test-generator.R
@@ -2,5 +2,12 @@ context("generator")
 
 test_that("name consists of LEFT and RIGHT", {
   name <- get_random_name()
+  
   split <- strsplit(name, '_', TRUE)
+  expect_length(split, 1)
+  
+  split <- split[[1]]
+  expect_length(split, 2)
+  expect_true(split[1] %in% LEFT)
+  expect_true(split[2] %in% RIGHT)
 })
